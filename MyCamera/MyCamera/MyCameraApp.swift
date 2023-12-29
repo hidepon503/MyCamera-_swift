a39356f2a6a7ac5ffce9a3363b3052e733adc50b
hidepon503 <blue.passion625@gmail.com>
2023-12-29 22:55:43 +0900

    1st

---
+++ MyCamera/MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 飯塚 秀幸 on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
