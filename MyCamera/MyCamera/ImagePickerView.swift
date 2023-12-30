e6be19d5479a779f2c2cd7dbf2ea52c69a3b761c
hidepon503 <blue.passion625@gmail.com>
2023-12-30 19:09:34 +0900

    フォトライブラリから写真をUPするところまで完成

---
+++ MyCamera/MyCamera/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by 飯塚 秀幸 on 2023/12/29.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
