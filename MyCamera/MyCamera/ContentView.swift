7c7b571e363683322e2ee31143cadeb2884325a7
hidepon503 <blue.passion625@gmail.com>
2023-12-31 00:53:04 +0900

    完成

--- MyCamera/MyCamera/ContentView.swift
+++ MyCamera/MyCamera/ContentView.swift
@@ -20,23 +20,14 @@ struct ContentView: View {
         VStack {
             //スペース追加
             Spacer()
-            //撮影した写真がある時
-            if let captureImage{
-                //撮影写真を表示
-                Image(uiImage: captureImage)
-                //リサイズ
-                    .resizable()
-                //アスペクト比
-                    .scaledToFit()
-            }
-            //スペース追加
-            Spacer()
             //「カメラを起動する」ボタン
             Button{
                 //ボタンをタップした時のアクション
                 //カメラが利用可能かチェック
                 if UIImagePickerController.isSourceTypeAvailable(.camera){
                     print("カメラは利用できます")
+                    //撮影写真を初期化する
+                    captureImage = nil
                     //カメラが使えるなら、isShowSheetをtrue
                     isShowSheet.toggle()
                 } else {
@@ -61,8 +52,13 @@ struct ContentView: View {
             //sheetを表示
             //isPresentedで指定した状態変数がtrueの時に実行
             .sheet(isPresented: $isShowSheet){
-                //UIImagePickerController（写真撮影）を表示
-                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+                if let captureImage{
+                    //撮影した写真がある→EffectViewを表示する
+                    EffectView(isShowSheet: $isShowSheet, captureImage: captureImage)
+                } else {
+                    //UIImagePickerController(写真撮影)を表示
+                    ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+                }
             }//『カメラを起動する』ボタンのsheetここまで
 
             //フォトライブラリーから写真を選択して使用する
@@ -99,28 +95,14 @@ struct ContentView: View {
                     }
                 }
             })//.onChangeここまで
-
-            //captureImageをアンラップする
-            if let captureImage {
-                //captureImageから共有する画像を生成する
-                let shareImage = Image(uiImage: captureImage)
-                //共有シート
-                ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("photo", image: shareImage)){
-                    //テキスト表示
-                    Text("SNSに投稿する")
-                    //横幅いっぱい
-                        .frame(maxWidth: .infinity)
-                    //高さ50ポイント
-                        .frame(height: 50)
-                    //背景を青色に指定
-                        .background(Color.blue)
-                    //文字色を白色に指定
-                        .foregroundStyle(Color.white)
-                    //上下左右に余白を追加
-                        .padding()
-                }//shareLinkここまで
-            }//アンラップここまで
         }//VStackここまで
+        //撮影した写真を保持する状態変数が変化したら実行する
+        .onChange(of: captureImage, initial: true, {oldValue, newValue in
+            if let _ = newValue {
+                //撮影した写真がある→EffectViewを表示する
+                isShowSheet.toggle()
+            }
+        })//.onChangeここまで
     }//bodyここまで
 }
 
