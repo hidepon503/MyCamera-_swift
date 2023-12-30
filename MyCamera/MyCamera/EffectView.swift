188a38d5d68d2a877940dfdbde50c49925038158
hidepon503 <blue.passion625@gmail.com>
2023-12-31 00:52:29 +0900

    完成

---
+++ MyCamera/MyCamera/EffectView.swift
@@ -0,0 +1,18 @@
+//
+//  EffectView.swift
+//  MyCamera
+//
+//  Created by 飯塚 秀幸 on 2023/12/30.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EffectView()
+}
